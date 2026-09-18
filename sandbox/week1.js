64404dda7b0d025ae481a441c9e5aed98badec1d
Tate's Linux Desktop <tmwaldo1@gmail.com>
2026-09-18 16:05:54 -0600

    W1 ponder

--- sandbox/week1.js
+++ sandbox/week1.js
@@ -1,1 +1,21 @@
 console.log("Hello, World!")
+
+
+const PI = 3.14;
+let radius = 3;
+
+
+const one = 1;
+const two = '2';
+console.log(one);
+console.log(two);
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "Tate";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
