01e45b8a30810418bc9c3313aa05fcdbc3f5de6f
Tate's Linux Desktop <tmwaldo1@gmail.com>
2026-09-24 15:45:10 -0600

    start provve

---
+++ prove/mission/mis.js
@@ -0,0 +1,15 @@
+
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        // code for changes to colors and logo
+    } else {
+        // code for changes to colors and logo
+    }
+}           
+                    
