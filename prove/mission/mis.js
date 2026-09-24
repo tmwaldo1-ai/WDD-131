f070324367a2d3d80425de1ca607d423794ea93f
Tate's Linux Desktop <tmwaldo1@gmail.com>
2026-09-24 15:55:42 -0600

    finish prove

--- prove/mission/mis.js
+++ prove/mission/mis.js
@@ -1,15 +1,18 @@
 
 let selectElem = document.querySelector('select');
 let logo = document.querySelector('img');
+const all = document.querySelector('body');
 
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        // code for changes to colors and logo
+        all.classList.add('dark')
+        logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-white.png')
     } else {
-        // code for changes to colors and logo
+        all.classList.remove('dark')
+        logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp')
     }
 }           
                     
